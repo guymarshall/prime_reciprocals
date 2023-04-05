9fb6fc9a01763a0c47141bc37c4180afd977ee26
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-04-05 12:30:05 +0100

    Change return type to HashMap<i32, i32>

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,7 +1,7 @@
 pub use std::collections::HashMap;
 
-fn combine_vectors_into_hashmap(keys: Vec<String>, values: Vec<i32>) -> HashMap<String, i32> {
-    let mut hash_map: HashMap<String, i32> = HashMap::new();
+fn combine_vectors_into_hashmap(keys: Vec<i32>, values: Vec<i32>) -> HashMap<i32, i32> {
+    let mut hash_map: HashMap<i32, i32> = HashMap::new();
     for (key, value) in keys.into_iter().zip(values.into_iter()) {
         hash_map.insert(key, value);
     }
