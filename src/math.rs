0bbde8fef949d4111c7d2ae778fa5eb592ee5085
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-04-05 12:47:39 +0100

    Add TODO for primes_up_to

--- src/math.rs
+++ src/math.rs
@@ -19,7 +19,7 @@ fn is_prime(number: &i32) -> bool {
 
 pub fn primes_up_to(number: i32) -> Vec<i32> {
     (2..=number).into_par_iter().filter(is_prime).collect::<Vec<i32>>()
-}
+} // TODO change to calculate "number" of primes, instead of primes "up to" number
 
 pub fn reciprocal_decimal_count(input: i32) -> i32 {
     let mut dividend: i32 = 1;
