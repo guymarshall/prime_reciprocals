20ea025a8cd8c74f13cd3656a2c7c10556b48952
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-04-05 12:33:18 +0100

    Improve code for reciprocal_repeating_digit_counts drastically

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,10 @@ fn main() {
     let number: i32 = user_input::input("Enter a number:");
     let primes: Vec<i32> = math::primes_up_to(number);
 
-    let mut reciprocal_repeating_digit_counts: Vec<i32> = Vec::with_capacity(number as usize);
-    (1..number).into_iter().for_each(|i| {
-        let element: i32 = primes[i as usize];
-        let repeating_digit_count: i32 = math::reciprocal_decimal_count(element);
-
-        reciprocal_repeating_digit_counts[i as usize] = repeating_digit_count;
-    });
+    let reciprocal_repeating_digit_counts: Vec<i32> = primes
+        .iter()
+        .map(|&prime| math::reciprocal_decimal_count(prime))
+        .collect();
 
     (0..number).into_iter().for_each(|i| {
         println!("Reciprocal of {} repeats after {} digits.", primes[i as usize], reciprocal_repeating_digit_counts[i as usize]);
