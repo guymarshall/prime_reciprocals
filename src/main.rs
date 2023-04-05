a957be1224f3a69d14f392ac6d9f7c0285c30297
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-04-05 12:36:00 +0100

    Add print_hashmap call to print primes_with_reciprocal_repeating_digit_counts

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,7 @@ fn main() {
         .map(|&prime| math::reciprocal_decimal_count(prime))
         .collect();
 
-    (0..number).into_iter().for_each(|i| {
-        println!("Reciprocal of {} repeats after {} digits.", primes[i as usize], reciprocal_repeating_digit_counts[i as usize]);
-    });
-
     let primes_with_reciprocal_repeating_digit_counts: HashMap<i32, i32> = utilities::combine_vectors_into_hashmap(primes, reciprocal_repeating_digit_counts);
+
+    utilities::print_hashmap(&primes_with_reciprocal_repeating_digit_counts);
 }
