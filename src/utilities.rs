431d1ac429e5613a19718fb0b9e97b6e8842d36f
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-04-05 12:43:07 +0100

    Remove print_hashmap - unused

--- src/utilities.rs
+++ src/utilities.rs
@@ -8,16 +8,6 @@ pub fn combine_vectors_into_hashmap(keys: Vec<i32>, values: Vec<i32>) -> HashMap
     hash_map
 }
 
-pub fn print_hashmap<K, V>(hash_map: &HashMap<K, V>)
-    where
-        K: std::fmt::Display,
-        V: std::fmt::Display,
-{
-    for (key, value) in hash_map.iter() {
-        println!("Reciprocal of {} repeats after {} digits.", key, value);
-    }
-}
-
 pub fn sort_hashmap_by_key<K: Ord, V>(hash_map: &HashMap<K, V>) -> Vec<(&K, &V)> {
     let mut sorted_pairs: Vec<(&K, &V)> = hash_map
         .into_iter()
