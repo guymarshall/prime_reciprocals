e2d218b32297c7ecb06490ac328c0210850a3021
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-04-05 12:30:48 +0100

    Make combine_vectors_into_hashmap public

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,6 +1,6 @@
-pub use std::collections::HashMap;
+use std::collections::HashMap;
 
-fn combine_vectors_into_hashmap(keys: Vec<i32>, values: Vec<i32>) -> HashMap<i32, i32> {
+pub fn combine_vectors_into_hashmap(keys: Vec<i32>, values: Vec<i32>) -> HashMap<i32, i32> {
     let mut hash_map: HashMap<i32, i32> = HashMap::new();
     for (key, value) in keys.into_iter().zip(values.into_iter()) {
         hash_map.insert(key, value);
