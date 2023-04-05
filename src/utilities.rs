b9f730053c44cf7d919ffe0e94e5a76e43cf4beb
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-04-05 12:37:33 +0100

    Add sort_hashmap_by_key to utilities.rs

--- src/utilities.rs
+++ src/utilities.rs
@@ -17,3 +17,11 @@ pub fn print_hashmap<K, V>(hash_map: &HashMap<K, V>)
         println!("Reciprocal of {} repeats after {} digits.", key, value);
     }
 }
+
+pub fn sort_hashmap_by_key<K: Ord, V>(hash_map: &HashMap<K, V>) -> Vec<(&K, &V)> {
+    let mut sorted_pairs: Vec<(&K, &V)> = hash_map
+        .into_iter()
+        .collect();
+    sorted_pairs.sort_by_key(|pair| pair.0);
+    sorted_pairs
+}
