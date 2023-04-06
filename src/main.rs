a1eb9161c92c59f28d904b98c2716b7d92ec207d
Guy Marshall <guymarshall9@gmail.com>
2023-04-06 22:46:28 +0100

    Add println to show when combining primes occurs

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ fn main() {
         .map(|&prime| math::reciprocal_decimal_count(prime))
         .collect();
 
+    println!("Combining primes and repeating digit counts into hashmap...");
     let primes_with_reciprocal_repeating_digit_counts: HashMap<i32, i32> = utilities::combine_vectors_into_hashmap(primes, reciprocal_repeating_digit_counts);
 
     let sorted_pairs: Vec<(&i32, &i32)> = utilities::sort_hashmap_by_key(&primes_with_reciprocal_repeating_digit_counts);
